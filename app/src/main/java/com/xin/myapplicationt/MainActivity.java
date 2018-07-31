59ed5dd7a33b302343e063be1f7ff66a1b20d83a
2840830311@qq.com <2840830311@qq.com>
2018-07-31 12:31:31 +0800

    001
    001

---
+++ app/src/main/java/com/xin/myapplicationt/MainActivity.java
@@ -0,0 +1,31 @@
+package com.xin.myapplicationt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToastUtil toastUtil = new ToastUtil(this);
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showToast("btn1");
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.showToast("btn2");
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/xin/myapplicationt/ToastUtil.java
@@ -0,0 +1,49 @@
+package com.xin.myapplicationt;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * 项目名称：鼎品
+ * 类描述：
+ * 创建人：${Peter}
+ * 创建时间： 2018/7/31.
+ */
+public class ToastUtil extends Toast {
+
+    public ToastUtil(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    private static Context mContext;
+
+    private static ToastUtil toast;
+
+    public static void showToast(String msg) {
+        try {
+            TextView textView = null;
+            View view = null;
+            if (view == null) {
+                view = LayoutInflater.from(mContext).inflate(R.layout.toast_view, null);
+                textView = (TextView) view.findViewById(R.id.textView1);
+            }
+            if (toast == null) {
+                toast = new ToastUtil(mContext);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 200);
+            }
+            textView.setText(msg);
+            toast.setView(view);
+            toast.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
